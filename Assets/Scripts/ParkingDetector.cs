c062d989eb0082c1ab8e0fdc9664dfa6232c0669
fierval <fierval@gmail.com>
2019-11-14 16:40:47 -0800

    Parking lot design

---
+++ Assets/Scripts/ParkingDetector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkingDetector : MonoBehaviour
+{
+    GameObject parkingBounds;
+    Renderer parkRender;
+    Bounds parkingBoundingBox;
+
+    private void Awake()
+    {
+        parkingBounds = transform.Find("ParkingBounds").gameObject;
+        parkRender = parkingBounds.GetComponent<Renderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parkingBoundingBox = parkRender.bounds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        parkingBoundingBox = parkRender.bounds;
+    }
+}
