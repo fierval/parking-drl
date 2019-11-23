08b1563e8ef0617fa5999fa07b45016746de320c
fierval <fierval@gmail.com>
2019-11-23 12:26:39 -0800

    balance

---
+++ Assets/Scripts/CarAgent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class CarAgent : Agent
+{
+    public override void InitializeAgent()
+    {
+        base.InitializeAgent();
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        base.AgentAction(vectorAction, textAction);
+    }
+}
