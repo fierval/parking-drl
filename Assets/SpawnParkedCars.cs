523f1f17dbf719dce16d3c423b2d0572f2e75516
fierval <fierval@gmail.com>
2019-11-28 13:59:56 -0800

    parking spots population start work

---
+++ Assets/SpawnParkedCars.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class SpawnParkedCars : MonoBehaviour
+{
+    Dictionary<GameObject, List<Vector3>> parkingSpots;
+    [SerializeField] GameObject[] carPrefabs;
+    [SerializeField] GameObject[] parkingLots;
+
+    private void Awake()
+    {
+        PopulateFreeSpots();
+        OccupyParkingSpots();
+    }
+
+    private void OccupyParkingSpots()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void PopulateFreeSpots()
+    {
+        // Populate the list of parking spots
+        foreach (var pl in parkingLots)
+        {
+            parkingSpots.Add(pl, new List<Vector3>());
+            foreach (Transform spot in pl.transform.GetChild(0) as Transform)
+            {
+                parkingSpots[pl].Add(spot.position);
+            }
+        }
+    }
+}
