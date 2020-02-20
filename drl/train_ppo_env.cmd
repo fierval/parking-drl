090a83536c29dac1e014c5261204eda4799c6838
fierval <fierval@gmail.com>
2020-02-19 19:50:36 -0800

    ppo trainer

---
+++ drl/train_ppo_env.cmd
@@ -0,0 +1,1 @@
+mlagents-learn config\ppo_trainer_config.yaml --env ..\env\cars.exe --run-id ppo-1 --keep-checkpoints 10 --train --width 800 --height 800 --quality-level 0 --time-scale 20  --curriculum=config\curricula\Parking
