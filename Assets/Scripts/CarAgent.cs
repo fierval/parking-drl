c19f9ca9ad7097f68739551ccc88d2434bbb291e
fierval <fierval@gmail.com>
2020-02-23 01:09:23 -0800

    reward experiments

--- Assets/Scripts/CarAgent.cs
+++ Assets/Scripts/CarAgent.cs
@@ -22,7 +22,7 @@ public enum Facing :int
 struct Rewards
 {
     public const float BaseReward = -1e-3f;
-    public const float DistanceWeight = 3.5e-4f;
+    public const float DistanceWeight = 1e-4f;
     // should line up with the parking spot
     public const float AngleWeight = 1e-4f;
     // should be as close to 0 as possible when parking
@@ -201,16 +201,17 @@ public class CarAgent : Agent
         }
 
         var distance = GetDistanceFromGoal();
+        var rewardDistance = (1f / (distance + 1e-7f)) * (1.4f - Mathf.Pow(distance, 1.1f));
 
         // small reward for getting closer to parking
         // and also turning towards it
         reward +=
-            NoDistanceIfCompleteParking() * (1f/(distance + 1e-10f)) * Rewards.DistanceWeight
+            NoDistanceIfCompleteParking() * rewardDistance * Rewards.DistanceWeight
             + Mathf.Abs(Mathf.Cos(angle)) * Rewards.AngleWeight
             + velocity.magnitude * Rewards.VelocityWeight;
 
         Monitor.Log("Distance", distance.ToString(), transform);
-        Monitor.Log("DistanceReversed", 1f / (distance + 1e-10f), transform);
+        Monitor.Log("RewardDistance", rewardDistance, transform);
         Monitor.Log("CosAngle", Mathf.Cos(angle), transform);
         Monitor.Log("Reward", reward.ToString(), transform);
         return reward;
