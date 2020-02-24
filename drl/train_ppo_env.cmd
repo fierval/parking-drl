1d5cc6b1e062b02459054d892dc50fcf725e3437
fierval <fierval@gmail.com>
2020-02-24 09:50:56 -0800

    config

--- drl/train_ppo_env.cmd
+++ drl/train_ppo_env.cmd
@@ -1,3 +1,3 @@
 rd models /q/s
 rd summaries /q/s
-mlagents-learn config\ppo_trainer_config.yaml --env ..\env\cars.exe --run-id ppo-1 --keep-checkpoints 10 --train --width 800 --height 800 --quality-level 0 --time-scale 20  --curriculum=config\curricula\Parking
+mlagents-learn config\ppo_trainer_config.yaml --env ..\env\cars.exe --run-id ppo-2 --keep-checkpoints 10 --train --width 800 --height 800 --quality-level 0 --time-scale 20  --curriculum=config\curricula\Parking
